9e65605e35190ec98ca470761b7dc192db42eb9e
Alendor22 <alendor6@gmail.com>
2020-10-16 00:24:59 -0400

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+solo = Admin.create(name:'Solomon Peña',email:'solomonraulpena@gmail.com',zip:11224)
+
+adeja = Admin.create(name:'Adeja Lendor',email:'adejalendor@gmail.com',zip:10452)
+
+20.times do
+  Project.create(title:Faker::Company.name,summary:Faker::Lorem.paragraphs,objective:Faker::Lorem.paragraphs,location:"#{Faker::FunnyName.name} street, brooklyn, NY",admin_id:Admin.all.sample.id)   
+end
+
+100.times do
+  Donor.create(name:Faker::FunnyName.name,email:"#{Faker::FunnyName.name}@#{Faker::FunnyName.name}.com",zip:Faker::Number.within(range: 10000..90000))
+end
+
+225.times do
+  Donation.create(donor_id:Donor.all.sample.id,project_id:Project.all.sample.id,amount:Faker::Commerce.price(range: 1..1000000.0, as_string: true))
+end
+
+puts('seeded successfully!...')
