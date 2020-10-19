295883895e1716edbe0dbb8cbc4e42637c98fed0
Alendor22 <alendor6@gmail.com>
2020-10-18 22:41:07 -0400

    add controller actions

--- app/controllers/api/v1/donations_controller.rb
+++ app/controllers/api/v1/donations_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::DonationsController < ApplicationController
+class Api::V1::DonationsController < ApiController
   before_action :set_donation, only: [:show, :update, :destroy]
 
   # GET /donations
